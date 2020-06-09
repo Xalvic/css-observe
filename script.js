fe3c65344e5590e03ee8f165f68d78a0e964c08d
Xalvic <puthukkuddymilan@gmail.com>
2020-06-10 02:24:27 +0530

    big updates

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const audi = document.querySelector(".audi");
+const vid = document.querySelector(".vid");
 let wave = document.querySelector(".wave");
 let wave2 = document.querySelector(".wave2");
 let text = document.querySelector(".start");
+let name = document.querySelector(".Name");
+let nav = document.querySelector(".navbar");
 let main = document.querySelector(".further-out");
 
 main.addEventListener("click", () => {
@@ -12,6 +15,13 @@ main.addEventListener("click", () => {
     text.classList.remove("dope");
   }, 9000);
   setTimeout(() => {
+    name.classList.add("Name-in");
+    nav.classList.add("nav-in");
+  }, 2500);
+  setTimeout(() => {
+    main.classList.add("move");
+  }, 300);
+  setTimeout(() => {
     wave.classList.remove("expand");
     wave2.classList.remove("expand");
   }, 2000);
@@ -21,6 +31,7 @@ const start = () => {
   // const key = document.querySelector('.crash1');
   // audi.currentTime = 0;
   audi.play();
+  vid.play();
   // key.classList.add('trigger');
 };
 // const stop = () => {
