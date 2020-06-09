3e1a68ffb3badd6cf8c39726d30a6af80f6cbb33
Xalvic <puthukkuddymilan@gmail.com>
2020-06-10 00:23:55 +0530

    updates

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const audi = document.querySelector(".audi");
 let wave = document.querySelector(".wave");
+let wave2 = document.querySelector(".wave2");
+let text = document.querySelector(".start");
 let main = document.querySelector(".further-out");
 
 main.addEventListener("click", () => {
   wave.classList.add("expand");
-
+  wave2.classList.add("expand");
+  text.classList.add("dope");
+  setTimeout(() => {
+    text.classList.remove("dope");
+  }, 9000);
   setTimeout(() => {
     wave.classList.remove("expand");
+    wave2.classList.remove("expand");
   }, 2000);
 });
 
